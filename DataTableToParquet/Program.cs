0e5c1787b804b465a24a2a0b899c6a180e23920b
Chuong Ho <chuongpqvn@gmail.com>
2024-01-30 17:02:57 +0800

    maintain

--- DataTableToParquet/Program.cs
+++ DataTableToParquet/Program.cs
@@ -4,8 +4,10 @@ using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using Parquet;
-using Parquet.Data;
+using Parquet.Schema;
+using Array = System.Array;
 using DataColumn = System.Data.DataColumn;
 using DBNull = System.DBNull;
 
@@ -18,31 +20,38 @@ namespace DataTableToParquet
 
         private const string OutputFilePath = "example.parquet";
 
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var dt = GenerateTestData();
-            var fields = GenerateSchema(dt);
+            await ExportToParquet(dt, OutputFilePath);
+        }
 
-            // Open the output file for writing
-            using (var stream = File.Open(OutputFilePath, FileMode.Create, FileAccess.Write))
+        private static async Task ExportToParquet(DataTable dataTable, string filePath)
+        {
+            DataField[] fields = GetSchema(dataTable);
+            ParquetSchema paquetSchema = new ParquetSchema(fields);
+            using (Stream fileStream = System.IO.File.OpenWrite(filePath))
             {
-                using (var writer = new ParquetWriter(new Schema(fields), stream))
+                using (ParquetWriter parquetWriter =
+                       await ParquetWriter.CreateAsync(paquetSchema, fileStream).ConfigureAwait(false))
                 {
-                    var startRow = 0;
+                    parquetWriter.CompressionMethod = CompressionMethod.Gzip;
+                    parquetWriter.CompressionLevel = System.IO.Compression.CompressionLevel.Optimal;
 
+                    var startRow = 0;
                     // Keep on creating row groups until we run out of data
-                    while (startRow < dt.Rows.Count)
+                    while (startRow < dataTable.Rows.Count)
                     {
-                        using (var rgw = writer.CreateRowGroup(RowGroupSize))
+                        using (var rgw = parquetWriter.CreateRowGroup())
                         {
                             // Data is written to the row group column by column
-                            for (var i = 0; i < dt.Columns.Count; i++)
+                            for (var i = 0; i < dataTable.Columns.Count; i++)
                             {
                                 var columnIndex = i;
 
                                 // Determine the target data type for the column
-                                var targetType = dt.Columns[columnIndex].DataType;
-                                if (targetType == typeof(DateTime)) targetType = typeof(DateTimeOffset);
+                                var targetType = dataTable.Columns[columnIndex].DataType;
+                                if (targetType == typeof(DateTime)) targetType = typeof(DateTime);
 
                                 // Generate the value type, this is to ensure it can handle null values
                                 var valueType = targetType.IsClass
@@ -50,13 +59,13 @@ namespace DataTableToParquet
                                     : typeof(Nullable<>).MakeGenericType(targetType);
 
                                 // Create a list to hold values of the required type for the column
-                                var list = (IList)typeof(List<>)
+                                IList list = (IList)typeof(List<>)
                                     .MakeGenericType(valueType)
-                                    .GetConstructor(Type.EmptyTypes)
+                                    .GetConstructor(Type.EmptyTypes)!
                                     .Invoke(null);
 
                                 // Get the data to be written to the parquet stream
-                                foreach (var row in dt.AsEnumerable().Skip(startRow).Take(RowGroupSize))
+                                foreach (var row in dataTable.AsEnumerable())
                                 {
                                     // Check if value is null, if so then add a null value
                                     if (row[columnIndex] == null || row[columnIndex] == DBNull.Value)
@@ -65,20 +74,21 @@ namespace DataTableToParquet
                                     }
                                     else
                                     {
-                                        // Add the value to the list, but if it's a DateTime then create it as a DateTimeOffset first
-                                        list.Add(dt.Columns[columnIndex].DataType == typeof(DateTime)
-                                            ? new DateTimeOffset((DateTime) row[columnIndex])
+                                        // Add the value to the list, but if it's a DateTime then create it as a DateTime
+                                        list.Add(dataTable.Columns[columnIndex].DataType == typeof(DateTime)
+                                            ? (DateTime)row[columnIndex]
                                             : row[columnIndex]);
                                     }
                                 }
 
                                 // Copy the list values to an array of the same type as the WriteColumn method expects
                                 // and Array
-                                var valuesArray = Array.CreateInstance(valueType, list.Count);
+                                Array valuesArray = Array.CreateInstance(valueType, list.Count);
                                 list.CopyTo(valuesArray, 0);
 
                                 // Write the column
-                                rgw.WriteColumn(new Parquet.Data.DataColumn(fields[i], valuesArray));
+                                await rgw.WriteColumnAsync(new Parquet.Data.DataColumn(paquetSchema.DataFields[i],
+                                    valuesArray)).ConfigureAwait(false);
                             }
                         }
 
@@ -88,34 +98,53 @@ namespace DataTableToParquet
             }
         }
 
-        /// <summary>
-        /// Creates a collection of Parquet fields from the <see cref="System.Data.DataTable"/>
-        /// </summary>
-        /// <param name="dt"></param>
-        /// <returns></returns>
-        private static List<DataField> GenerateSchema(DataTable dt)
+        public static DataField[] GetSchema(DataTable dataTable)
         {
-            var fields = new List<DataField>(dt.Columns.Count);
-
-            foreach (DataColumn column in dt.Columns)
+            var fields = new List<DataField>();
+            foreach (DataColumn column in dataTable.Columns)
             {
-                // Attempt to parse the type of column to a parquet data type
-                var success = Enum.TryParse<DataType>(column.DataType.Name, true, out var type);
-
-                // If the parse was not successful and it's source is a DateTime then use a DateTimeOffset, otherwise default to a string
-                if (!success && column.DataType == typeof(DateTime))
+                if (column.DataType == typeof(byte[]))
                 {
-                    type = DataType.DateTimeOffset;
+                    fields.Add(new DataField(column.ColumnName, typeof(string),true));
                 }
-                else if (!success)
+                else if(column.DataType== typeof(int))
                 {
-                    type = DataType.String;
+                    // nullable int
+                    fields.Add(new DataField(column.ColumnName, typeof(int),true));
+                }
+                else if (column.DataType == typeof(string))
+                {
+                    fields.Add(new DataField(column.ColumnName, typeof(string),true));
+                }
+                else if (column.DataType == typeof(double))
+                {
+                    fields.Add(new DataField(column.ColumnName, typeof(double),true));
+                }
+                else if (column.DataType == typeof(bool))
+                {
+                    fields.Add(new DataField(column.ColumnName, typeof(bool),true));
+                }
+                else if (column.DataType == typeof(Int32))
+                {
+                    fields.Add(new DataField(column.ColumnName, typeof(Int32),true));
+                }
+                else if (column.DataType == typeof(Int64))
+                {
+                    fields.Add(new DataField(column.ColumnName, typeof(Int64),true));
+                }
+                else if (column.DataType == typeof(DateTime))
+                {
+                    fields.Add(new DataField(column.ColumnName, typeof(DateTime),true));
+                }
+                // null
+                else if (column.DataType == typeof(DBNull))
+                {
+                    fields.Add(new DataField(column.ColumnName, typeof(string),true));
                 }
 
-                fields.Add(new DataField(column.ColumnName, type));
             }
 
-            return fields;
+            return fields.ToArray();
         }
 
         /// <summary>
@@ -136,30 +165,35 @@ namespace DataTableToParquet
 
             var rand = new Random();
 
-            var forenames = new List<string> { "Celaena", "Gabe", "Rose", "Chris", "Girton", "Nick", "Aiana", "Julia", "Curtis", "Manuela", "Spike" };
+            var forenames = new List<string>
+            {
+                "Celaena", "Gabe", "Rose", "Chris", "Girton", "Nick", "Aiana", "Julia", "Curtis", "Manuela", "Spike"
+            };
             var surnames = new List<string> { "Smith", "Curts", "Power", "Stark", "Robinson", "Askew", "Maximus" };
             var ages = new List<int?> { 19, 23, 47, 71, null, 27 };
-            var lastSeen = new List<DateTime> { new DateTime(2018, 9, 14), new DateTime(2019, 7, 13), new DateTime(1418, 5, 21) };
+            var lastSeen = new List<DateTime>
+                { new DateTime(2018, 9, 14), new DateTime(2019, 7, 13), new DateTime(1418, 5, 21) };
             var scores = Enumerable.Range(1, 10).Select(_ => rand.NextDouble()).ToList();
 
             var sampleData = from f in forenames
-                             from s in surnames
-                             from a in ages
-                             from ls in lastSeen
-                             from sc in scores
-                             select new
-                             {
-                                 Name = $"{f} {s}",
-                                 Age = a,
-                                 LastSeen = ls,
-                                 Score = sc
-                             };
+                from s in surnames
+                from a in ages
+                from ls in lastSeen
+                from sc in scores
+                select new
+                {
+                    Name = $"{f} {s}",
+                    Age = a,
+                    LastSeen = ls,
+                    Score = sc
+                };
 
             var index = 1;
 
             foreach (var item in sampleData)
             {
-                dt.Rows.Add(dt.NewRow().ItemArray = new object[] { index++, item.Name, item.Age, item.LastSeen, item.Score });
+                dt.Rows.Add(dt.NewRow().ItemArray = new object[]
+                    { index++, item.Name, item.Age, item.LastSeen, item.Score });
             }
 
             return dt;
